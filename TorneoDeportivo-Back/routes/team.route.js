4d36f7594c3c33d96a8df3e0aacdc7fec2a02ef0
Diego <dgonzalez-2019203@kinal.edu.gt>
2021-07-11 05:03:45 -0600

    Se agrego controller teams

--- TorneoDeportivo-Back/routes/team.route.js
+++ TorneoDeportivo-Back/routes/team.route.js
@@ -2,9 +2,24 @@
 
 var express = require('express');
 var teamController = require('../controllers/team.controller');
+var mdAuth = require('../middlewares/authenticated');
+
+var connectMultiparty = require('connect-multiparty');
+var mdUpload = connectMultiparty({ uploadDir: './uploads/leagues'});
+var api = express.Router();
 
 var api = express.Router();
 
-api.post('/createTeam', teamController.createTeam); //Guardar un jugador.
+api.post ('/saveTeam', teamController.saveTeam);
+api.put('/updateTeam/:id', teamController.updateTeam);
+api.put('/removeTeam', teamController.removeTeam);
+api.put('/uploadTeam/:id',[mdUpload] ,teamController.uploadTeam);
+api.get("/getImageTeam/:fileName",[mdUpload],teamController.getImageTeam);
+api.post ('/findTeam', teamController.findTeam);
+api.put('/setTeamLeague/:id', teamController.setTeamLeague);
+api.get("/getLeagueTeam/:id", [mdAuth.ensureAuth], teamController.getLeagueTeam );
+api.get("/getTeams", [mdAuth.ensureAuth, mdAuth.ensureAuthAdminLeague],teamController.getTeams);
+
+
 
 module.exports = api;
